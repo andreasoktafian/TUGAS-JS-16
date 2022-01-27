ca5d91081ab10e34699e2fc8e1c4ad232d1ae77f
andreas <andreas.oktafiann@gmail.com>
2022-01-27 14:18:33 +0700

    TUGAS JS 16

---
+++ tugas16.js
@@ -0,0 +1,10 @@
+function minMax() {
+  var angka = [45, 100, 99, 3, 2, 4, 1, 2, 3, 19, 22, 14, 10];
+
+  console.log("Nilai Minimal : ", Math.min(45, 100, 99, 3, 2, 4, 1, 2, 3, 19, 22, 14, 10));
+  console.log("Nilai Maximal : ", Math.max(45, 100, 99, 3, 2, 81, 22, 55, 1, 2, 3, 4, 5));
+
+  return;
+}
+
+minMax();
